0ebbc0d67a12bb044fd773c6816b838a3ed5b53e
Athiin <116104526+AthosTelini@users.noreply.github.com>
2024-03-30 15:10:42 -0300

    Update script.js

--- Aula 12 ex/script.js
+++ Aula 12 ex/script.js
@@ -1,19 +1,1 @@
 
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    // var hora = data.getHours()
-    var hora = 14
-    msg.innerHTML= `Agora são ${hora} horas`
-
-    if (hora >= 0 && hora < 12) {
-        alert("Bom dia");
-        document.body.style.backgroundImage = "url('Sapinho/Dia.jpg')";
-    } else if (hora >= 12 && hora <= 18) {
-        alert("Boa Tarde");
-        document.body.style.backgroundImage = "url('Sapinho/tarde.jpg')";
-    } else {
-        document.body.style.backgroundImage = "url('Sapinho/noite.jpg')";
-    }
-}
